bf408fb577327f5264c6fb59526487ed4054c6fa
analusorbara <sorbaraanalu@gmail.com>
2025-04-13 11:39:53 -0300

    Final test

--- app/src/main/java/com/example/clevertapt/MainActivity.kt
+++ app/src/main/java/com/example/clevertapt/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.clevertapt
 
+import android.app.NotificationManager
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.util.Log.VERBOSE
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,19 +19,33 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.clevertap.android.sdk.CleverTapAPI
+import com.clevertap.android.sdk.PushPermissionResponseListener
 import com.example.clevertapt.ui.theme.CleverTapTTheme
 import java.util.Date
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), PushPermissionResponseListener {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1001)
+            }
+        }
+
         val cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
+
         CleverTapAPI.setDebugLevel(VERBOSE)
         CleverTapAPI.onActivityResumed(this)
 
         super.onCreate(savedInstanceState)
+
+        cleverTapDefaultInstance?.apply {
+            enableDeviceNetworkInfoReporting(true)
+            registerPushPermissionNotificationResponseListener(this@MainActivity)
+        }
 
         genProfile()
 
@@ -32,16 +53,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             CleverTapTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    MainScreen(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding), onClick = { buttonClick() }
+                        modifier = Modifier.padding(innerPadding),
+                        onEvent = { buttonClick() },
+                        onNotification = { /*fakeNotification()*/ }
                     )
                 }
             }
         }
     }
 
-    fun genProfile() {
+    /*private fun fakeNotification() {
+        val bundle = Bundle().apply {
+            putString("wzrk_acct_id", "TEST-WWR-KR5-KW7Z")
+            putString("nm", "Grab 'em on Myntra's Maxessorize Sale")
+            putString("nt", "Ye dil ❤️️ maange more accessories?")
+            putString("pr", "")
+            putString("wzrk_pivot", "")
+            putString("wzrk_sound", "true")
+            putString("wzrk_cid", "BRTesting")
+            putString("wzrk_clr", "#ed732d")
+            putString("wzrk_nms", "Grab 'em on Myntra's Maxessorize Sale")
+            putString("wzrk_pid", (10000_00000..99999_99999).random().toString())
+            putString("wzrk_rnv", "true")
+            putString("wzrk_ttl", "1627731067")
+            putString("wzrk_push_amp", "false")
+            putString("wzrk_bc", "")
+            putString("wzrk_bi", "2")
+            putString("wzrk_bp", "https://imgur.com/6DavQwg.jpg")
+            putString("wzrk_dl", "")
+            putString("wzrk_dt", "FIREBASE")
+            putString("wzrk_id", "1627639375_20210730")
+            putString("wzrk_pn", "true")
+        }
+
+        Thread {
+            CleverTapAPI.createNotification(applicationContext, bundle)
+        }.start()
+        Thread {
+            CleverTapAPI.createNotification(applicationContext, bundle)
+        }.start()
+    }
+    just for test - Github example
+    */
+
+    private fun genProfile() {
         val cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
 
         // each of the below mentioned fields are optional
@@ -51,7 +108,8 @@ class MainActivity : ComponentActivity() {
         profileUpdate["Email"] = "clevertap+sorbaraanalu@gmail.com" // Email address of the user
         profileUpdate["Phone"] = "+14155551234" // Phone (with the country code, starting with +)
         profileUpdate["Gender"] = "F" // Can be either M or F
-        profileUpdate["DOB"] = Date(1989, 2,8) // Date of Birth. Set the Date object to the appropriate value first
+        profileUpdate["DOB"] =
+            Date(1989, 2, 8) // Date of Birth. Set the Date object to the appropriate value first
 // optional fields. controls whether the user will be sent email, push etc.
 // optional fields. controls whether the user will be sent email, push etc.
         profileUpdate["MSG-email"] = false // Disable email notifications
@@ -68,35 +126,66 @@ class MainActivity : ComponentActivity() {
         cleverTapDefaultInstance?.pushProfile(profileUpdate)
     }
 
-    fun buttonClick() {
+    private fun buttonClick() {
         val cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
 
         val prodViewedAction = HashMap<String, Any>()
         prodViewedAction["Product ID"] = 1
-        prodViewedAction["Product Image"] = "https://d35fo82fjcw0y8.cloudfront.net/2018/07/26020307/customer-success-clevertap.jpg"
+        prodViewedAction["Product Image"] =
+            "https://d35fo82fjcw0y8.cloudfront.net/2018/07/26020307/customer-success-clevertap.jpg"
         prodViewedAction["Product Name"] = "CleverTap"
 
         cleverTapDefaultInstance?.pushEvent("Product viewed", prodViewedAction)
     }
+
+    override fun onPushPermissionResponse(accepted: Boolean) {
+        Log.i(TAG, "InApp---> response() called  $accepted")
+        if (accepted) {
+            Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show()
+
+            //For Android 13+ we need to create notification channel after notification permission is accepted
+            CleverTapAPI.createNotificationChannel(
+                this, "BRTesting", "Core",
+                "Core notifications", NotificationManager.IMPORTANCE_MAX, true
+            )
+
+            CleverTapAPI.createNotificationChannel(
+                this, "PTTesting", "Push templates",
+                "All push templates", NotificationManager.IMPORTANCE_MAX, true
+            )
+        } else {
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
-    Column {
+fun MainScreen(name: String,
+               modifier: Modifier = Modifier,
+               onEvent: () -> Unit = {},
+               onNotification: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = "CleverTap SDK Test $name!",
-            modifier = modifier
+            text = "CleverTap SDK Test $name!"
         )
-        Button(onClick = onClick) {
+        Button(onClick = onEvent) {
             Text("Click to send a product viewed event")
         }
+       /*Button(onClick = onNotification) {
+            Text("Click to send a push notification")
+        }*/
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     CleverTapTTheme {
-        Greeting("Android", onClick = {})
+        MainScreen("Android")
     }
 }
